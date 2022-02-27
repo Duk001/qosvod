6a9f69a4b29278ae20c041e74812dd6b1c591539
Filip Antkowiak <af44259@wi.zut.edu.pl>
2022-02-27 23:50:45 +0100

    small changes to frontend, added unlimited active session number per user

--- frontend-vue/piqosvod/src/host.js
+++ frontend-vue/piqosvod/src/host.js
@@ -1,4 +1,5 @@
 export default {
     'prod': 'https://piqosvod.azurewebsites.net/',
-    'dev': 'http://127.0.0.1:11000/',
+    'dev': 'http://192.168.1.107:10000/', //http://127.0.0.1:10000/
   }
+  //http://192.168.1.107/
